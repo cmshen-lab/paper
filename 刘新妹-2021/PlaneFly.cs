66ca1b3bddadc455598bd2f391a02844df607fb1
cmshen-lab <77827199+cmshen-lab@users.noreply.github.com>
2021-01-24 17:55:39 +0800

    Add files via upload

---
+++ 刘新妹-2021/PlaneFly.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaneFly : MonoBehaviour {
+    public float forward_speed = 13.0f;
+    public float back_speed = 8.0f;
+    public float rotation_speed = 100;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.W))
+        {
+            this.transform.Translate(Vector3.forward * forward_speed * Time.deltaTime);
+
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            this.transform.Translate(Vector3.forward * back_speed * -1 * Time.deltaTime);
+
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            this.transform.Rotate(this.transform.up * rotation_speed * -1 * Time.deltaTime);
+
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            this.transform.Rotate(this.transform.up * rotation_speed * Time.deltaTime);
+
+        }
+    }
+}
